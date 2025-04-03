d0d69af780f1dc891794c356a41d8d039cfdd7ed
richard garzon <garzon.r@gmail.com>
2025-04-02 21:15:14 -0400

    shared state

--- examples/hello-redis.rs
+++ examples/hello-redis.rs
@@ -4,7 +4,7 @@ use mini_redis::{client, Result};
 async fn main() -> Result<()> {
     // Open a connection to the mini-redis address.
     let mut client = client::connect("127.0.0.1:6379").await?;
-
+    println!("connected");
     // Set the key "hello" with value "world"
     client.set("hello", "world".into()).await?;
 
