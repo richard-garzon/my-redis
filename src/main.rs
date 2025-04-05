582f6545d8d4da98144f8ab8e6adbacd7d05d413
richard garzon <garzon.r@gmail.com>
2025-04-04 23:54:24 -0400

    channels

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,55 @@
 use bytes::Bytes;
 use mini_redis::{Connection, Frame};
 use std::collections::HashMap;
+use std::hash::{DefaultHasher, Hash, Hasher};
 use std::sync::{Arc, Mutex};
 use tokio::net::{TcpListener, TcpStream};
 
-type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+pub struct ShardedDb {
+    db: Arc<Vec<Mutex<HashMap<String, Bytes>>>>,
+}
+
+impl ShardedDb {
+    pub fn new(num_shards: usize) -> Self {
+        let mut db = Vec::with_capacity(num_shards);
+        for _ in 0..num_shards {
+            db.push(Mutex::new(HashMap::new()));
+        }
+
+        ShardedDb { db: Arc::new(db) }
+    }
+
+    pub fn insert(&self, key: String, value: Bytes) {
+        let mut hasher = DefaultHasher::new();
+        key.hash(&mut hasher);
+
+        // feels bad to cast u64 as usize here, but we're modulo-ing...
+        let mut shard = self.db[hasher.finish() as usize % self.db.len()]
+            .lock()
+            .unwrap();
+        shard.insert(key, value);
+    }
+
+    pub fn get(&self, key: String) -> Option<Bytes> {
+        let mut hasher = DefaultHasher::new();
+        key.hash(&mut hasher);
+
+        let shard = self.db[hasher.finish() as usize % self.db.len()]
+            .lock()
+            .unwrap();
+        // I wonder if this is still zero-copy clone()?
+        return shard.get(&key).cloned();
+    }
+}
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     println!("Listening...");
 
-    let db = Arc::new(Mutex::new(HashMap::new()));
-
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-        let db = db.clone();
+        let db = ShardedDb::new(10);
         println!("accepted...");
         tokio::spawn(async move {
             process(socket, db).await;
@@ -23,7 +57,7 @@ async fn main() {
     }
 }
 
-async fn process(socket: TcpStream, db: Db) {
+async fn process(socket: TcpStream, db: ShardedDb) {
     use mini_redis::Command::{self, Get, Set};
 
     let mut connection = Connection::new(socket);
@@ -31,13 +65,11 @@ async fn process(socket: TcpStream, db: Db) {
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                let mut db = db.lock().unwrap();
                 db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
-                let db = db.lock().unwrap();
-                if let Some(value) = db.get(cmd.key()) {
+                if let Some(value) = db.get(cmd.key().to_string()) {
                     Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
